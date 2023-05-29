cf6c4f5b93b3039123c8a3ea3f5a148c17069437
YuuY314 <rafaelyu0202@gmail.com>
2023-05-29 15:57:10 -0200

    Basis Structure

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Story Maker Studio</title>
+    <link rel="stylesheet" href="css/styles.css?v=<?php echo time(); ?>">
+</head>
+<body>
+    <header>
+        <a href="index.php"><img src="" alt=""></a>
+        <nav>
+            <ul>
+                <li><a href="index.php">Projeto</a></li>
+                <li><a href="about.php">Sobre</a></li>
+                <li><a href="contact.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            
+        </section>
+    </main>
+    <footer>
+        <p>&copy Story Maker Studio</p>
+        Desenvolvido por <a href="https://github.com/YuuY314" target="_blank">Rafael Yu</a>
+    </footer>
+</body>
+</html>
